da5ed976f86db6569933db200201bbccaee5c1c1
yzp531 <yzp531@163.com>
2018-09-17 17:20:45 +0800

    init

---
+++ MyARouter/app/src/main/java/com/alibaba/android/arouter/demo/MainApp.java
@@ -0,0 +1,13 @@
+package com.alibaba.android.arouter.demo;
+
+import android.app.Application;
+
+import com.alibaba.android.arouter.launcher.ARouter;
+
+public class MainApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ARouter.init(this);
+    }
+}
